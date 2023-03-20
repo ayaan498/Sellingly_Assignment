56107d43e4ca9a597a874974f2f9e3f0e6564513
Ayaan Gani <77957630+ayaan498@users.noreply.github.com>
2023-03-21 03:45:11 +0530

    Firestore updated

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 interface BankDetail {
-  id?: string;
+  id: string;
   bankName: string;
   accountNumber: string;
 }
@@ -37,24 +37,35 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.firestore.collection<BankDetail>('bank-form').valueChanges().subscribe((data: BankDetail[]) => {
+    this.firestore.collection<BankDetail>('bankDetails').valueChanges().subscribe((data: BankDetail[]) => {
       this.bankDetails = data;
+      console.log(this.bankDetails); // log the bankDetails array to check that it's correct
     });
-    
   }
+  
   
   
   removeBankDetails(index: number) {
-    const docId = this.bankDetails[index].id;
-    this.firestore.collection('bankDetails').doc(docId).delete()
-      .then(() => {
-        console.log('Bank detail deleted successfully!');
-        this.bankDetails.splice(index, 1);
+    const accountNumber = this.bankDetails[index].accountNumber;
+    this.firestore.collection('bankDetails', ref => ref.where('accountNumber', '==', accountNumber)).get()
+      .toPromise()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          doc.ref.delete().then(() => {
+            console.log('Bank detail deleted successfully!');
+            this.bankDetails.splice(index, 1);
+          }).catch(error => {
+            console.error('Error deleting bank detail: ', error);
+          });
+        });
       })
-      .catch((error) => {
-        console.error('Error deleting bank detail: ', error);
+      .catch(error => {
+        console.error('Error finding bank detail: ', error);
       });
   }
+  
+  
+  
   
   logout() {
     this.auth.logout();
